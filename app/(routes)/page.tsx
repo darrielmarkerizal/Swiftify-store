f830ad427602b40f76c145bbcb8e5f4c7a2fd483
Darriel Markerizal <darrielmarkerizal@gmail.com>
2024-01-04 20:55:55 +0700

    billboard and featured product

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,27 @@
-const HomePage = () => {
-    return <div>Hello Store</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard(
+        "82da245d-ba0c-43ac-9072-6d71f88c9b24"
+    );
+
+    const products = await getProducts({ isFeatured: true });
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products} />
+            </div>
+        </Container>
+    );
 };
 
 export default HomePage;
